b636790d8150bfe697c647cb9b4addbeb3b8354d
vfpomer <114563375+vfpomer@users.noreply.github.com>
2025-07-02 23:02:45 +0200

    app

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -25,7 +25,7 @@ def load_data():
         df_inmobiliario = pd.read_csv("data/valencia_vivienda_limpio.csv")
         df_delincuencia = pd.read_csv("data/crimenValencia.csv", sep=';')
         df_barcelona = pd.read_csv("data/barcelona_limpio_completo.csv")
-        df_barcelona_inversores = pd.read_csv("data\barcelona_inversores.csv")
+        df_barcelona_inversores = pd.read_csv("data/barcelona_inversores.csv")
         return df_valencia, df_inmobiliario, df_delincuencia,df_barcelona, df_barcelona_inversores
     except Exception as e:
         st.error(f"Error al cargar los datos: {e}")
