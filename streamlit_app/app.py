e37abf55f412998d4f4043aa9f427d94c66e0740
Vanesa Fernández <114563375+vfpomer@users.noreply.github.com>
2025-07-04 13:44:40 +0200

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -15,6 +15,7 @@ from maps_utils import crear_mapa_oportunidades, crear_mapa_precios_valencia, cr
 from maps_utils import crear_mapa_precios_valencia, crear_heatmap_ocupacion_valencia,  display_interactive_map
 import streamlit.components.v1 as components
 from maps_utils import display_interactive_map, display_image, crear_evolucion_reseñas,mostrar_mapa_correlaciones,mostrar_matriz_correlacion,mostrar_relacion_precio_calificacion,mostrar_mapa_perfiles, crear_mapa_valencia,mostrar_mapa_con_fallback,mostrar_imagen, mostrar_imagen_con_fallback,mostrar_mapa, mostrar_mapa_con_fallback
+import copy
 
 #Ruta base al directorio raíz del proyecto (2 niveles arriba si estás en streamlit_app/)
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
